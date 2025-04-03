a7cde981808df9960c8fd931bfdcd3e48408057e
KusugaShiro <fajri.a.f027@gmail.com>
2025-04-03 21:57:07 +0800

    latihan array

--- latihan2.cpp
+++ latihan2.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
+string kelas[30];
+int pos = 0; // posisi data dimasukkan
+void inputData(string nama); //
+void outputData(); // deklarasi procedur tampilkan data
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+
+int main(){
+
+    //cout << "hello world";
+    
+    inputData("indomi goreng plus telor"); // pemanggilan procedure
+    outputData();
+    inputData("ayam bacem sambal mata");// pemanggilan procedure
+    outputData();
+    inputData("lontong sayur lauk haruan"); // pemanggilan procedure
+    outputData();
+    return 0;
+}
+
+void inputData(string nama){
+    kelas [pos]=nama;// masukkan data kedalam array
+    pos++; // geser posisi masukkan data
+    }
+
+void outputData(){
+    for (int i = 0; i < pos; i++)
+    {
+        cout << kelas[i];
+        if (i<pos-1)
+        {
+            cout <<","; //koma pemisah data
+         }else{
+             cout <<"."<<endl; //titik pemisah data akhir dan pindah baris
+            }
+    }
+
 }
